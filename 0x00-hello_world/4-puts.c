14635e243a96a869a6f84398a0ed7e99896996db
ABOUBAKR95 <aboubakrezziyani@gmail.com>
2023-06-15 18:29:30 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Description: a programme that print a line
- *
- * Returne: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
